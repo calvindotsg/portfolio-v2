a4f65e38d1b24a99bbd3864177480cf8bcca4ce8
Calvin <68703834+calvindotsg@users.noreply.github.com>
2024-05-12 18:27:10 +0800

    initial commit, work in progress

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,10 +1,46 @@
-export const LINKS = {
-  github: "https://github.com/Ladvace",
-  linkedin: "https://www.linkedin.com/in/gianmarco-cavallo/",
-  medium: "https://ladvace.medium.com/",
-  discord: "https://discordapp.com/users/163300027618295808",
-  dribble: "https://dribbble.com/Ladvace_Jace",
+export const LINKS: {
+  github: string
+  linkedin: string
+  instagram: string
+  strava: string
+  telegram: string
+  email: string
+} = {
+  github: "https://github.com/calvindotsg",
+  linkedin: "https://www.linkedin.com/in/calvin-loh/",
+  instagram: "https://www.instagram.com/calvindotsg/",
+  strava: "https://www.strava.com/athletes/37641259/",
+  telegram: "https://t.me/calvindotsg/",
+  email: "mailto:hello@calvin.sg?subject=Let's%20get%20in%20touch!%20A%20quick%20question&body=Hey!%0A%0AJust%20popped%20by%20your%20website%20calvin.sg%20and%20had%20a%20quick%20question%3A%0A%0AName%3A%20%0AMessage%3A%0A%0ALooking%20forward%20to%20your%20response!%0A%0ACheers%2C"
 };
+
+export const CAREER = {
+
+}
+
+export const ABOUT_ME = {
+
+}
+
+export const WELCOME = {
+
+}
+
+export const NOW = {
+
+}
+
+export const COUNTRIES_VISITED = {
+
+}
+
+export const FOOTER = {
+
+}
+
+export const METADATA = {
+
+}
 
 export const loaderAnimation = [
   ".loader",
