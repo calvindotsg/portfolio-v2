1e0b92592bf9a390942092c1a771585d28171b43
Calvin <68703834+calvindotsg@users.noreply.github.com>
2024-05-24 23:20:34 +0800

    removed redundant og-image.jpg

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -107,7 +107,7 @@ export const METADATA: {
     description: "Hustle hustling at startup, probably cycling when you find me",
     site_url: "https://calvin.sg/",
     name: "Calvin",
-    image_url: "https://calvin.sg/og-image.jpg",
+    image_url: "https://calvin.sg/preview.png",
     address_locality: "Singapore",
     address_country: "SG",
     email_obfuscated: "hello[at]calvin.sg"
