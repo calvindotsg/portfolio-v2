76c8ed2968ce604fa5fe99d1743ec00ee73c6b2c
Calvin <68703834+calvindotsg@users.noreply.github.com>
2024-08-24 15:34:41 +0800

    improve typing

--- src/components/ThemeSwitcher.svelte
+++ src/components/ThemeSwitcher.svelte
@@ -20,7 +20,7 @@
 <!-- Theme Toggle Button -->
 <button
         aria-label="Toggle Theme"
-        class={`custom-btn text-xl max-h-[40px] max-w-[60px] shadow-custom shadow-[var(--shadow)] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] text-[var(--text)] px-5 py-1 border border-[var(--accent)] hover:text-[var(--accent)] transition-colors duration-100 ease-in-out bg-[var(--background)] cursor-pointer rounded-lg`}
+        class="custom-btn text-xl max-h-[40px] max-w-[60px] shadow-custom shadow-[var(--shadow)] active:shadow-none active:translate-x-[3px] active:translate-y-[3px] text-[var(--text)] px-5 py-1 border border-[var(--accent)] hover:text-[var(--accent)] transition-colors duration-100 ease-in-out bg-[var(--background)] cursor-pointer rounded-lg"
         on:click={toggleTheme}>
     {theme === 'light' ? '☀' : '☽'}
     <slot/>
