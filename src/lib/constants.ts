acc7f80d06a5313c4f2b32bc78add5667da779af
Calvin <68703834+calvindotsg@users.noreply.github.com>
2025-05-21 23:35:24 -0500

    Fix syntax error

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -57,7 +57,7 @@ export const GOAL: {
     measurable_unit: string
 } = {
     total_goal: 3000,
-    current_progress: 252,1,
+    current_progress: 252.1,
     progress_last_year: 2528,
     website_url: "https://www.strava.com/athletes/37641259/",
     goal_name: "Cycling",
