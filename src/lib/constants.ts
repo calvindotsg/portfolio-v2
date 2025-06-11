ffb05ea957ef463a3d42870ea2b6c25f18222b63
Calvin <68703834+calvindotsg@users.noreply.github.com>
2025-06-11 08:51:08 +0800

    Update cycling progress in constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -57,7 +57,7 @@ export const GOAL: {
     measurable_unit: string
 } = {
     total_goal: 3000,
-    current_progress: 333.4,
+    current_progress: 378.9,
     progress_last_year: 2528,
     website_url: "https://www.strava.com/athletes/37641259/",
     goal_name: "Cycling",
